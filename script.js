2e7aec60eb34e2617c476177e65983998cda973e
gmate563 <197899324+gmate563@users.noreply.github.com>
2025-07-03 16:54:39 +0200

    Make code easier

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const container = document.querySelector(".container");
 
 function gridCreator(numberOfSquaresPerSide){
+    container.textContent = "";
+    
+    const squareSize = 640 / numberOfSquaresPerSide;
+
     for (let index = 0; index < (numberOfSquaresPerSide * numberOfSquaresPerSide); index++) {
         const div = document.createElement("div");
-        container.appendChild(div);
-        div.classList.add(`div-${index}`);
-        document.querySelector(`.div-${index}`).addEventListener("mouseover", () => {
-            document.querySelector(`.div-${index}`).style.backgroundColor="red";
+        div.classList.add("cell");
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor="red";
         });
+        
+        container.appendChild(div);
+
     }
 }
 
@@ -16,10 +25,9 @@ gridCreator(16);
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
     let input = prompt("Type in a number between 1 and 100", 16);
-    container.textContent = "";
+    // container.textContent = "";
     if (input <= 100 && input > 0){
         gridCreator(input);
-        prevGridNumberOfSquaresPerSide = input;
     }
     else {
         alert("The number provided is not acceptable.")
