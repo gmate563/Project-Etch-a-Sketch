2a8e70c91157f98b51fe5d73365a576a9e849a2c
gmate563 <197899324+gmate563@users.noreply.github.com>
2025-07-03 13:23:42 +0200

    Add hover effect

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const container = document.querySelector(".container");
 for (let index = 0; index < 256; index++) {
     const div = document.createElement("div");
     container.appendChild(div);
+    div.classList.add(`div-${index}`);
+    document.querySelector(`.div-${index}`).addEventListener("mouseover", () => {
+        document.querySelector(`.div-${index}`).style.backgroundColor="red";
+    });
 }
